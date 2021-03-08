54b2488813ca43487879da3ceaa04e8ded99b24d
serek4 <jacezarem@gmail.com>
2021-03-08 17:59:29 +0100

    mounting file system in plugin constructor added mount check in mounting routine

--- src/plugins/basicFS.cpp
+++ src/plugins/basicFS.cpp
@@ -2,11 +2,17 @@
 
 
 BasicFS::BasicFS() {
+	_fsStarted = setup();
 }
 BasicFS::~BasicFS() {
 }
 
 bool BasicFS::setup() {
+	FSInfo FsInfo;
+	if (LittleFS.info(FsInfo)) {
+		BASICFS_PRINTLN("LittleFS already mounted!");
+		return true;
+	}
 	while (!LittleFS.begin()) {
 		BASICFS_PRINTLN("LittleFS mount failed!");
 		return false;

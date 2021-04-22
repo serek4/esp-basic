16d418587d162bf2bc899151bc66ce5c0187281a
serek4 <jacezarem@gmail.com>
2021-04-22 12:52:32 +0200

    added esp32 FS(LITTLEFS) support

--- src/plugins/basicFS.cpp
+++ src/plugins/basicFS.cpp
@@ -2,12 +2,26 @@
 
 
 BasicFS::BasicFS() {
-	_fsStarted = setup();
+#if defined(ARDUINO_ARCH_ESP8266)
+	_fsStarted = setup();    //* crash on esp32
+#endif
 }
 BasicFS::~BasicFS() {
 }
 
 bool BasicFS::setup() {
+#ifdef ARDUINO_ARCH_ESP32
+	if (esp_littlefs_mounted("spiffs")) {
+		BASICFS_PRINTLN("LittleFS already mounted!");
+		return true;
+	}
+	if (!LITTLEFS.begin()) {
+		BASICFS_PRINTLN("LITTLEFS mount failed!");
+		return false;
+	}
+	BASICFS_PRINTLN("LITTLEFS mounted!");
+	return true;
+#elif defined(ARDUINO_ARCH_ESP8266)
 	FSInfo FsInfo;
 	if (LittleFS.info(FsInfo)) {
 		BASICFS_PRINTLN("LittleFS already mounted!");
@@ -19,4 +33,5 @@ bool BasicFS::setup() {
 	}
 	BASICFS_PRINTLN("LittleFS mounted!");
 	return true;
+#endif
 }
